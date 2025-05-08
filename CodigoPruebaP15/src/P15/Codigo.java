39ed90174c3877a006005e391e80930b847d17bc
jorgecarpinterodaw <jorge.carpintero@plazacastilla.salesianas.org>
2025-05-08 12:14:21 +0200

    Add files via upload

---
+++ CodigoPruebaP15/src/P15/Codigo.java
@@ -0,0 +1,23 @@
+package P15;
+
+import java.util.Scanner;
+
+public class Codigo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub  
+		Scanner sc=new Scanner (System.in); 
+		String nombre, edad; 
+		
+		System.out.println("Esto es una prueba GIT"); 
+		System.out.println("Tu nombre:  "); 
+		nombre = sc.nextLine();    
+		System.out.println("Tu nombre es: " + nombre); 
+		
+		System.out.println("Tu edad:  ");  
+		edad= sc.nextLine();     
+		System.out.println("Tienes: " + edad + " años."); 
+		
+	}
+
+}
